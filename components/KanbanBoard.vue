371483705e232dd440bc495fd5615fc1d925ac99
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-03-23 19:10:20 +0100

    Add drag columns

--- components/KanbanBoard.vue
+++ components/KanbanBoard.vue
@@ -1,30 +1,40 @@
 <template>
-    <div class="flex items-start gap-4 overflow-x-auto">
-        <div 
-            class="column bg-gray-200 p-5 rounded min-w-[250px]"
-            v-for="column in columns"
-            ::key="column.id"
+    <div>
+        <draggable 
+            v-model="columns"
+            group="columns"
+            :animation="150"
+            handle=".drag-handle"
+            item-key="id"
+            class="flex items-start gap-4 overflow-x-auto"
         >
-            <header class="font-bold mb-4">
-                {{ column.title }}
-            </header>
-            <KanbanTask 
-            v-for="task in column.tasks"
-            :key="task.id"
-            :task="task"
-            >
-                {{ task.title }}
-            </KanbanTask>
-            <footer>
-                <button class="text-gray-500">+ Add a Card</button>
-            </footer>
-        </div>
+            <template #item="{ element: column }: { element: Column }">
+                <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
+                    <header class="font-bold mb-4">
+                        <DragHandle />
+                        {{ column.title }}
+                    </header>
+                    <KanbanTask 
+                    v-for="task in column.tasks"
+                    :key="task.id"
+                    :task="task"
+                    >
+                        {{ task.title }}
+                    </KanbanTask>
+                    <footer>
+                        <button class="text-gray-500">+ Add a Card</button>
+                    </footer>
+                </div>
+            </template>
+        </draggable>
     </div>
 </template>
 
 <script setup lang="ts">
 import { nanoid } from 'nanoid';
+import draggable from "vuedraggable";
 import type { Column } from '../types/index';
+import DragHandle from '../components/DragHandle.vue';
 
 const columns = ref<Column[]>([
     {

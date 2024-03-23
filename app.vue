9f80234bf2e0f7374851a5009c2519316d811d0e
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-03-23 17:54:45 +0100

    Create KanbanBoard component

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <KanbanBoard />
   </div>
 </template>
---
+++ components/KanbanBoard.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div 
+            v-for="column in columns"
+            ::key="column.id"
+        >
+            <header>
+                {{ column.title }}
+            </header>
+            <p 
+                v-for="task in column.tasks"
+                :key="task.id"
+            >
+                {{ task.title }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { nanoid } from 'nanoid';
+import type { Column } from '../types/index';
+
+const columns = ref<Column[]>([
+    {
+        id: nanoid(),
+        title: "Backlog",
+        tasks: [
+            {
+                id: nanoid(),
+                title: "Create Dashboard",
+                createdAt: new Date(),
+            },
+            {
+                id: nanoid(),
+                title: "Create Landing Page",
+                createdAt: new Date(),
+            },
+            {
+                id: nanoid(),
+                title: "Revise Storybook",
+                createdAt: new Date(),
+            },
+        ],
+    },
+    {
+        id: nanoid(),
+        title: "To Do",
+        tasks: [],
+    },
+    {
+        id: nanoid(),
+        title: "In Progress",
+        tasks: [],
+    },
+    {
+        id: nanoid(),
+        title: "In Revision",
+        tasks: [],
+    },
+    {
+        id: nanoid(),
+        title: "Done",
+        tasks: [],
+    },
+]);
+
+</script>

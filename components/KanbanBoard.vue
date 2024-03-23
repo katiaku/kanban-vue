5c10308b7699b872bb1e9ca1a01491f3b37f3d5a
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-03-23 20:27:56 +0100

    Add edit, add and remove columns functionalities

--- components/KanbanBoard.vue
+++ components/KanbanBoard.vue
@@ -1,18 +1,24 @@
 <template>
-    <div>
+    <div class="flex items-start overflow-x-auto gap-4">
         <draggable 
             v-model="columns"
             group="columns"
             :animation="150"
             handle=".drag-handle"
             item-key="id"
-            class="flex items-start gap-4 overflow-x-auto"
+            class="flex items-start gap-4"
         >
             <template #item="{ element: column }: { element: Column }">
                 <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
                     <header class="font-bold mb-4">
                         <DragHandle />
-                        {{ column.title }}
+                        <input
+                            class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+                            @keyup.enter="($event.target as HTMLInputElement).blur()"
+                            @keydown.backspace="column.title === '' ? (columns = columns.filter(c => c.id !== column.id)) : null"
+                            type="text"
+                            v-model="column.title"
+                        />
                     </header>
                     <draggable 
                         v-model="column.tasks"
@@ -36,6 +42,12 @@
                 </div>
             </template>
         </draggable>
+        <button
+            @click="createColumn"
+            class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+        >
+            + Add Another Column
+        </button>
     </div>
 </template>
 
@@ -90,6 +102,22 @@ const columns = ref<Column[]>([
 ]);
 
 const alt = useKeyModifier("Alt");
+
+function createColumn() {
+    const column: Column = {
+        id: nanoid(),
+        title: "",
+        tasks: [],
+    }
+    columns.value.push(column);
+    nextTick(() => {
+        (
+            document.querySelector(
+                ".column:last-of-type .title-input"
+            ) as HTMLInputElement
+        ).focus();
+    });
+};
 
 </script>
 

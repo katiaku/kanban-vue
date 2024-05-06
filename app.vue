e30ba0533509f123ec953cf80f6956705d3a06ec
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-06 13:30:46 +0200

    Add the app logo

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
-    <h1 class="text-4xl text-white flex items-center mb-10">
+  <div class="p-10 h-[100vh] bg-slate-100 overflow-auto">
+    <h1 class="text-4xl text-slate-800 flex items-center mb-10">
       <img
-        width="200"
+        width="70"
         class="mr-3"
-        src=""
-        alt="app logo"
+        src="./public/assets/images/kanban-vue-logo.png"
+        alt="kanban-vue app logo"
       />
-      Welcome to KanbanVue Board!
+      Welcome to your KanbanVue Board!
     </h1>
     <KanbanBoard />
   </div>
